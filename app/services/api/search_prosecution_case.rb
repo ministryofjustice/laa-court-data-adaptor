315b0c49534760ed055250488943876c036396ae
Arlston Fernandes <gpg@arlston.com>
2019-11-27 13:42:24 +0000

    Search by date of next hearing

--- app/services/api/search_prosecution_case.rb
+++ app/services/api/search_prosecution_case.rb
@@ -4,13 +4,14 @@ module Api
   class SearchProsecutionCase < ApplicationService
     include CommonPlatformConnection
 
-    def initialize(prosecution_case_reference: nil, national_insurance_number: nil, arrest_summons_number: nil, name: nil, date_of_birth: nil)
+    def initialize(prosecution_case_reference: nil, national_insurance_number: nil, arrest_summons_number: nil, name: nil, date_of_birth: nil, date_of_next_hearing: nil)
       @url = '/prosecutionCases'
       @prosecution_case_reference = prosecution_case_reference
       @national_insurance_number = national_insurance_number
       @arrest_summons_number = arrest_summons_number
       @name = name
       @date_of_birth = date_of_birth
+      @date_of_next_hearing = date_of_next_hearing
     end
 
     def call
@@ -32,6 +33,7 @@ module Api
     def request_params
       return { prosecutionCaseReference: prosecution_case_reference } if prosecution_case_reference.present?
       return { arrestSummonsNumber: arrest_summons_number } if arrest_summons_number.present?
+      return { name: name, dateOfNextHearing: date_of_next_hearing } if date_of_next_hearing.present?
       return { name: name, dateOfBirth: date_of_birth } if date_of_birth.present?
 
       { nationalInsuranceNumber: national_insurance_number }
@@ -50,6 +52,6 @@ module Api
       end
     end
 
-    attr_reader :prosecution_case_reference, :national_insurance_number, :response, :url, :arrest_summons_number, :name, :date_of_birth
+    attr_reader :prosecution_case_reference, :national_insurance_number, :response, :url, :arrest_summons_number, :name, :date_of_birth, :date_of_next_hearing
   end
 end
