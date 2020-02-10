5896f478de3870475f7a389b5b83dab689779bd5
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2020-02-10 14:31:08 +0000

    Remove unnecessary prosecution_case endpoint
    There are currently two prosecution_case endpoints defined. One
    (outside the API namespace) was a spike for integrating with MLRA. It
    is no longer unnecessary. This commit removes the controller, route and
    various specs.

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :status, only: [:index]
   resources :hearings, only: [:create]
-  resources :prosecution_cases, only: [:index]
 end
--- spec/routing/prosecution_cases_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe ProsecutionCasesController, type: :routing do
-  describe 'routing' do
-    it 'routes to #index' do
-      expect(get: '/prosecution_cases').to route_to('prosecution_cases#index')
-    end
-  end
-end
