b01802c3ea2c21593773210ae0a7433a0b26ced2
Arlston Fernandes <gpg@arlston.com>
2020-04-30 09:10:35 +0100

    Add Indicies and foreign key for prosecution case - defendant - offence relationship table

---
+++ db/migrate/20200429163050_add_prosecution_case_indices.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class AddProsecutionCaseIndices < ActiveRecord::Migration[6.0]
+  def change
+    add_index :prosecution_case_defendant_offences, :prosecution_case_id, name: 'index_case_defendant_offences_on_prosecution_case'
+    add_index :prosecution_case_defendant_offences, :defendant_id
+    add_index :prosecution_case_defendant_offences, :offence_id
+    add_foreign_key :prosecution_case_defendant_offences, :prosecution_cases
+  end
+end
