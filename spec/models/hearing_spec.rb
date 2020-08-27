03c4e058708198e0bb05639c184540f0971f623d
Chris Groves <chris_groves87@hotmail.co.uk>
2020-08-27 17:05:08 +0100

    Amend tests against Hearing model

--- spec/models/hearing_spec.rb
+++ spec/models/hearing_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Hearing, type: :model do
   subject { hearing }
 
   context 'the hearing has not been resulted' do
-    it { expect(hearing.resulted?).to eq false }
+    it { expect(subject).to_not be_persisted }
   end
 
   describe 'validations' do
@@ -31,7 +31,8 @@ RSpec.describe Hearing, type: :model do
     it { expect(hearing.providers).to be_empty }
     it { expect(hearing.provider_ids).to be_empty }
     it { expect(hearing.judge_names).to be_empty }
-    
+    it { expect(hearing).to be_persisted }
+
     context 'hearing events' do
       let(:hearing_day) { '2020-04-17' }
 
