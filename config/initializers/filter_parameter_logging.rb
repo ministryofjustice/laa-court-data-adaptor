84b7303a764c45c904a55bc55d8b52fa57099011
Arlston Fernandes <gpg@arlston.com>
2020-11-02 11:53:47 +0000

    Omit frozen strings from being filtered, eg: true/false Since we are using String#replace to filter vales, strings like true/false, can cause errors to be thrown along the lines of FrozenError: can't modify frozen String: "true" Since there isn't any inherent value in filtering true/false, we ignore these

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,6 +4,8 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 filter_sensitive_info = ->(_key, value) do
+  next if value.frozen?
+
   next if /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/i.match?(value.to_s)
 
   value.to_s.replace '[FILTERED]'
