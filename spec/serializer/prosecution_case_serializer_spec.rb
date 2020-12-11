f060467917a84ee0af2c2bb52262c8ce3fcffda6
Arthur Ashman <arthur.ashman@digital.justice.gov.uk>
2020-12-11 14:59:06 +0000

    respond to rubocop errors

--- spec/serializer/prosecution_case_serializer_spec.rb
+++ spec/serializer/prosecution_case_serializer_spec.rb
@@ -25,5 +25,4 @@ RSpec.describe ProsecutionCaseSerializer do
     it { expect(relationship_hash[:hearing_summaries][:data]).to eq([{ id: "HEARING-UUID", type: :hearing_summaries }]) }
     it { expect(relationship_hash[:hearings][:data]).to eq([{ id: "HEARING-UUID", type: :hearings }]) }
   end
-  
 end
