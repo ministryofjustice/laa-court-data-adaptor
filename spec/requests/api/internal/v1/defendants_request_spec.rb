099bcfa66d2288e14dfa90a2bc07a11c5cdf17f3
Laurent Curau <laurent.curau@digital.justice.gov.uk>
2021-03-31 12:55:03 +0200

    Decommission defendants#update endpoint

--- spec/requests/api/internal/v1/defendants_request_spec.rb
+++ spec/requests/api/internal/v1/defendants_request_spec.rb
@@ -9,18 +9,6 @@ RSpec.describe "Api::Internal::V1::Defendants", type: :request, swagger_doc: "v1
   let(:token) { access_token }
   let(:id) { "23d7f10a-067a-476e-bba6-bb855674e23b" }
   let(:include) {}
-  let(:defendant) do
-    {
-      data: {
-        type: "defendants",
-        attributes: {
-          user_name: "johnDoe",
-          unlink_reason_code: 1,
-          unlink_other_reason_text: "",
-        },
-      },
-    }
-  end
 
   around do |example|
     Sidekiq::Testing.fake! do
@@ -29,66 +17,6 @@ RSpec.describe "Api::Internal::V1::Defendants", type: :request, swagger_doc: "v1
   end
 
   path "/api/internal/v1/defendants/{id}" do
-    patch("patch defendant relationships") do
-      description "Delete an LAA reference from Common Platform case"
-      consumes "application/json"
-      tags "Internal - available to other LAA applications"
-      security [{ oAuth: [] }]
-
-      response(202, "Accepted") do
-        parameter name: :id, in: :path, required: true, type: :uuid,
-                  schema: {
-                    '$ref': "defendant.json#/definitions/id",
-                  },
-                  description: "The unique identifier of the defendant"
-
-        parameter name: :defendant, in: :body, required: true, type: :object,
-                  schema: {
-                    '$ref': "defendant.json#/definitions/resource_to_unlink",
-                  },
-                  description: "Object containing the user_name, unlink_reason_code and unlink_other_reason_text"
-
-        parameter "$ref" => "#/components/parameters/transaction_id_header"
-
-        let(:Authorization) { "Bearer #{token.token}" }
-
-        before do
-          expect(UnlinkLaaReferenceWorker).to receive(:perform_async).with(String, id, "johnDoe", 1, "").and_call_original
-        end
-
-        run_test!
-      end
-
-      context "when data is bad" do
-        response("400", "Bad Request") do
-          let(:Authorization) { "Bearer #{token.token}" }
-          let(:id) { "X" }
-
-          parameter "$ref" => "#/components/parameters/transaction_id_header"
-
-          before do
-            expect(UnlinkLaaReferenceWorker).not_to receive(:perform_async)
-          end
-
-          run_test!
-        end
-      end
-
-      context "when request is unauthorized" do
-        response("401", "Unauthorized") do
-          let(:Authorization) { nil }
-
-          parameter "$ref" => "#/components/parameters/transaction_id_header"
-
-          before do
-            expect(UnlinkLaaReferenceWorker).not_to receive(:perform_async)
-          end
-
-          run_test!
-        end
-      end
-    end
-
     get("fetch a defendant by ID") do
       description "find a defendant where it exists within Court Data Adaptor"
       consumes "application/json"
