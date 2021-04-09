d7bfff3b2269577881158d5529e1b123e2493e8f
Arthur Ashman <arthur.ashman@digital.justice.gov.uk>
2021-04-09 14:42:11 +0100

    adds ruby class wrappers for HMCTS common platform objects
    classes made for plea, delegated powers and lesser or alternative offence objects

---
+++ spec/models/hmcts_common_platform/delegated_powers_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe HmctsCommonPlatform::DelegatedPowers, type: :model do
+  let(:data) { JSON.parse(file_fixture("delegated_powers.json").read).deep_symbolize_keys }
+  let(:delegated_powers) { described_class.new(data) }
+
+  it "has a user id" do
+    expect(delegated_powers.user_id).to eql("sd22b110-0pbc-3136-a076-e4bb40d0a986")
+  end
+
+  it "has a first name" do
+    expect(delegated_powers.first_name).to eql("Ada")
+  end
+
+  it "has last name" do
+    expect(delegated_powers.last_name).to eql("Lovelace")
+  end
+end
