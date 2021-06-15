15ad8cab007264a0b91f1bfa1b83da7227e91591
Laurent Curau <laurent.curau@digital.justice.gov.uk>
2021-06-15 12:15:38 +0200

    Revert "Handle delayed jobs with hearing_id arg"
    This reverts commit 8c5ba6d0078668c3e0c0abb99dad554fe034d234.

--- app/services/hearings_creator.rb
+++ app/services/hearings_creator.rb
@@ -1,23 +1,10 @@
 # frozen_string_literal: true
 
 class HearingsCreator < ApplicationService
-  attr_reader :hearing_resulted_data
+  attr_reader :hearing_resulted
 
   def initialize(hearing_resulted_data:)
-    @hearing_resulted_data = hearing_resulted_data
-  end
-
-  def hearing_resulted
-    # :nocov:
-    if hearing_resulted_data.is_a?(String)
-      hearing_id = hearing_resulted_data
-      HmctsCommonPlatform::HearingResulted.new(Hearing.find(hearing_id).body)
-    end
-    # :nocov:
-
-    if hearing_resulted_data.is_a?(Hash)
-      HmctsCommonPlatform::HearingResulted.new(hearing_resulted_data)
-    end
+    @hearing_resulted = HmctsCommonPlatform::HearingResulted.new(hearing_resulted_data)
   end
 
   def call
