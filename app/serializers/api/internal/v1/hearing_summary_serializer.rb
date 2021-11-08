3c5e9ca903871578fff5b355af1ed1a58e441d2f
Laurent Curau <laurent.curau@digital.justice.gov.uk>
2021-11-08 12:19:37 +0100

    Expose defence counsels on hearing summary (API v1)

--- app/serializers/api/internal/v1/hearing_summary_serializer.rb
+++ app/serializers/api/internal/v1/hearing_summary_serializer.rb
@@ -5,6 +5,7 @@ module Api
     module V1
       class HearingSummarySerializer
         include JSONAPI::Serializer
+
         attributes :hearing_type, :estimated_duration
 
         attribute :hearing_days do |hearing_summary|
@@ -18,6 +19,8 @@ module Api
             name: hearing_summary.court_centre.name,
           }
         end
+
+        has_many :defence_counsels
       end
     end
   end
