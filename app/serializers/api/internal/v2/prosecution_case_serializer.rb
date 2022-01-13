e7c37c8b13e7beabb4f46cdd408a0fb08e127d0d
Laurent Curau <laurent.curau@digital.justice.gov.uk>
2022-01-13 12:44:32 +0100

    Fix error no method "urn"

--- app/serializers/api/internal/v2/prosecution_case_serializer.rb
+++ app/serializers/api/internal/v2/prosecution_case_serializer.rb
@@ -7,7 +7,7 @@ module Api
         include JSONAPI::Serializer
         set_type :prosecution_cases
 
-        attributes :urn
+        attributes :prosecution_case_reference
 
         has_many :defendants, record_type: :defendants
       end
--- spec/serializers/api/internal/v2/prosecution_case_serializer_spec.rb
+++ spec/serializers/api/internal/v2/prosecution_case_serializer_spec.rb
@@ -3,22 +3,23 @@
 RSpec.describe Api::Internal::V2::ProsecutionCaseSerializer do
   subject { described_class.new(prosecution_case).serializable_hash }
 
-  let(:prosecution_case) do
-    instance_double("HMCTSCommonPlatform::ProsecutionCase",
-                    id: "UUID",
-                    urn: "AAA",
-                    defendant_ids: %w[DEFENDANT-UUID])
-  end
+  let(:prosecution_case_summary_data) { JSON.parse(file_fixture("prosecution_case_summary/all_fields.json").read) }
+  let(:prosecution_case) { ProsecutionCase.new(body: prosecution_case_summary_data) }
 
   context "with attributes" do
     let(:attribute_hash) { subject[:data][:attributes] }
 
-    it { expect(attribute_hash[:urn]).to eq("AAA") }
+    it { expect(attribute_hash[:prosecution_case_reference]).to eq("30DI0570888") }
   end
 
   context "with relationships" do
     let(:relationship_hash) { subject[:data][:relationships] }
 
-    it { expect(relationship_hash[:defendants][:data]).to eq([{ id: "DEFENDANT-UUID", type: :defendants }]) }
+    it do
+      expect(relationship_hash[:defendants][:data]).to eq([
+        { id: "b760daba-0d38-4bae-ad57-fbfd8419aefe", type: :defendants },
+        { id: "562dc2de-2258-4476-8be4-69cf15623f83", type: :defendants },
+      ])
+    end
   end
 end
