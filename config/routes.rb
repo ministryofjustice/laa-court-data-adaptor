a7ce0cf63f309edfb6373972fa24865b06e3bbc0
Laurent Curau <laurent.curau@digital.justice.gov.uk>
2022-02-04 17:14:04 +0100

    PATCH /laa_references/:defendant_id

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
 
       api_version(module: "V2", path: { value: "v2" }) do
         resources :prosecution_cases, only: [:index]
-        resources :laa_references, only: %i[create destroy], param: :defendant_id
+        resources :laa_references, only: %i[create update], param: :defendant_id
         resources :defendants, only: %i[update show]
         resources :representation_orders, only: [:create]
         resources :hearing_results, only: [:show]
--- spec/requests/api/internal/v2/laa_references_spec.rb
+++ spec/requests/api/internal/v2/laa_references_spec.rb
@@ -53,16 +53,16 @@ RSpec.describe "api/internal/v2/laa_references", type: :request, swagger_doc: "v
       response(202, "Accepted") do
         around do |example|
           Sidekiq::Testing.fake!
-          VCR.use_cassette("laa_reference_recorder/update") do
+          VCR.use_cassette("laa_reference_recorder/post") do
             example.run
           end
         end
 
         parameter name: :laa_reference, in: :body, required: false, type: :object,
-                  schema: {
-                    "$ref": "laa_reference.json#/definitions/new_resource",
-                  },
-                  description: "The LAA issued reference to the application. CDA expects a numeric number, although HMCTS allows strings"
+          schema: {
+            "$ref": "laa_reference.json#/definitions/new_resource",
+          },
+          description: "The LAA issued reference to the application. CDA expects a numeric number, although HMCTS allows strings"
 
         parameter "$ref" => "#/components/parameters/transaction_id_header"
 
@@ -145,6 +145,75 @@ RSpec.describe "api/internal/v2/laa_references", type: :request, swagger_doc: "v
 
           expect(response.body).to include("is not a valid uuid")
           expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+    end
+
+    path "/api/internal/v2/laa_references/{defendant_id}" do
+      patch("update laa_reference") do
+        description "Update an LAA reference (mark as unlinked)"
+        consumes "application/json"
+        tags "Internal - available to other LAA applications"
+        security [{ oAuth: [] }]
+
+        response(202, "Accepted") do
+          around do |example|
+            Sidekiq::Testing.fake!
+            VCR.use_cassette("laa_reference_recorder/update") do
+              example.run
+            end
+          end
+
+          parameter name: :defendant_id, in: :path, required: true, type: :uuid,
+            schema: {
+              "$ref": "defendant.json#/definitions/id",
+            },
+            description: "The unique identifier of the defendant"
+
+          parameter name: :laa_reference, in: :body, required: false, type: :object,
+            schema: {
+              "$ref": "laa_reference.json#/definitions/new_resource",
+            },
+            description: "The LAA issued reference to the application. CDA expects a numeric number, although HMCTS allows strings"
+
+          parameter "$ref" => "#/components/parameters/transaction_id_header"
+
+          let(:Authorization) { "Bearer #{token.token}" }
+
+          before do
+            expect(UnlinkLaaReferenceWorker).to receive(:perform_async).with("XYZ", defendant_id, "JaneDoe", 1, "")
+          end
+
+          run_test!
+        end
+
+        context "when data is bad" do
+          response("400", "Bad Request") do
+            let(:Authorization) { "Bearer #{token.token}" }
+            let(:defendant_id) { "X" }
+
+            parameter "$ref" => "#/components/parameters/transaction_id_header"
+
+            before do
+              expect(UnlinkLaaReferenceWorker).not_to receive(:perform_async)
+            end
+
+            run_test!
+          end
+        end
+
+        context "when request is unauthorized" do
+          response("401", "Unauthorized") do
+            let(:Authorization) { nil }
+
+            parameter "$ref" => "#/components/parameters/transaction_id_header"
+
+            before do
+              expect(UnlinkLaaReferenceWorker).not_to receive(:perform_async)
+            end
+
+            run_test!
+          end
         end
       end
     end
