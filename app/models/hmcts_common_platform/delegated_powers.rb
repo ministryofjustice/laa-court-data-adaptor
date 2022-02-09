9c28dc5813856ab6df8b9724da0d2fa01ca9a9f2
Laurent Curau <laurent.curau@digital.justice.gov.uk>
2022-02-09 17:31:59 +0100

    DelegatedPowers #to_json

--- app/models/hmcts_common_platform/delegated_powers.rb
+++ app/models/hmcts_common_platform/delegated_powers.rb
@@ -19,5 +19,25 @@ module HmctsCommonPlatform
     def last_name
       data[:lastName]
     end
+
+    def to_json(*_args)
+      return {} if attrs.all? { |_k, v| v.blank? }
+
+      attrs
+    end
+
+  private
+
+    def attrs
+      @attrs ||= to_builder.attributes!
+    end
+
+    def to_builder
+      Jbuilder.new do |dp|
+        dp.user_id user_id
+        dp.first_name first_name
+        dp.last_name last_name
+      end
+    end
   end
 end
