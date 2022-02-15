8720da345260900dc699ecf9d7bbbdd4723dfe85
Laurent Curau <laurent.curau@digital.justice.gov.uk>
2022-02-15 14:12:36 +0100

    Defendant #to_json

--- spec/models/hmcts_common_platform/defendant_spec.rb
+++ spec/models/hmcts_common_platform/defendant_spec.rb
@@ -8,185 +8,75 @@ RSpec.describe HmctsCommonPlatform::Defendant, type: :model do
       expect(data).to match_json_schema(:defendant)
     end
 
-    it "has an ID" do
-      expect(defendant.id).to eql("70d0817b-2a48-4751-b4a9-b6323874a310")
+    it "generates a JSON representation of the data" do
+      expect(defendant.to_json["id"]).to eql("70d0817b-2a48-4751-b4a9-b6323874a310")
+      expect(defendant.to_json["prosecution_case_id"]).to eql("4028f014-3a57-4e59-b014-f4d4502f7b73")
+      expect(defendant.to_json["proceedings_concluded"]).to be false
+      expect(defendant.to_json["defendant_details"]).to be_present
+      expect(defendant.to_json["offences"]).to be_present
+      expect(defendant.to_json["judicial_results"]).to be_present
+      expect(defendant.to_json["defence_organisation"]).to be_present
+      expect(defendant.to_json["legal_aid_status"]).to eql("aid status")
+      expect(defendant.to_json["is_youth"]).to be false
     end
 
-    it "has a proceedings concluded" do
-      expect(defendant.proceedings_concluded).to be(false)
-    end
-
-    it "has an ASN" do
-      expect(defendant.arrest_summons_number).to eql("TFL1")
-    end
-
-    it "has a first name" do
-      expect(defendant.first_name).to eql("John")
-    end
-
-    it "has a last name" do
-      expect(defendant.last_name).to eql("Yundt")
-    end
-
-    it "has a date of birth" do
-      expect(defendant.date_of_birth).to eql("1990-01-01")
-    end
-
-    it "has a NINO" do
-      expect(defendant.nino).to eql("AA123456C")
-    end
-
-    it "has documentation language needs" do
-      expect(defendant.documentation_language_needs).to eql("ENGLISH")
-    end
-
-    it "has address 1" do
-      expect(defendant.address_1).to eql("Address Line 1")
-    end
-
-    it "has address 2" do
-      expect(defendant.address_2).to eql("Address Line 2")
-    end
-
-    it "has address 3" do
-      expect(defendant.address_3).to eql("Address Line 3")
-    end
-
-    it "has address 4" do
-      expect(defendant.address_4).to eql("Address Line 4")
-    end
-
-    it "has address 5" do
-      expect(defendant.address_5).to eql("Address Line 5")
-    end
-
-    it "has a postcode" do
-      expect(defendant.postcode).to eql("SW1H 9EA")
-    end
-
-    it "has a home phone" do
-      expect(defendant.phone_home).to eql("000-000-0000")
-    end
-
-    it "has a work phone" do
-      expect(defendant.phone_work).to eql("111-111-1111")
-    end
-
-    it "has a mobile phone" do
-      expect(defendant.phone_mobile).to eql("222-222-2222")
-    end
-
-    it "has a primary email" do
-      expect(defendant.email_primary).to eql("primary@example.com")
-    end
-
-    it "has a secondary email" do
-      expect(defendant.email_secondary).to eql("secondary@example.com")
-    end
-
-    it "has offence ids" do
-      expect(defendant.offence_ids).to eql(%w[b8ffe7db-f994-45a0-9c06-9ec953eeffd7 b8ffe7db-f994-45a0-9c06-9ec953eeff99])
-    end
-
-    it "has offences" do
-      expect(defendant.offences).to all(be_an(HmctsCommonPlatform::Offence))
-    end
-
-    it "has judicial result ids" do
-      expect(defendant.judicial_result_ids).to eql(%w[be225605-fc15-47aa-b74c-efb8629db58e])
-    end
-
-    it "has judicial results" do
-      expect(defendant.judicial_results).to all(be_an(HmctsCommonPlatform::JudicialResult))
-    end
+    it { expect(defendant.id).to eql("70d0817b-2a48-4751-b4a9-b6323874a310") }
+    it { expect(defendant.prosecution_case_id).to eql("4028f014-3a57-4e59-b014-f4d4502f7b73") }
+    it { expect(defendant.proceedings_concluded).to be(false) }
+    it { expect(defendant.legal_aid_status).to eql("aid status") }
+    it { expect(defendant.is_youth).to be false }
+    it { expect(defendant.arrest_summons_number).to eql("TFL1") }
+    it { expect(defendant.first_name).to eql("John") }
+    it { expect(defendant.last_name).to eql("Yundt") }
+    it { expect(defendant.date_of_birth).to eql("1990-01-01") }
+    it { expect(defendant.nino).to eql("AA123456C") }
+    it { expect(defendant.documentation_language_needs).to eql("ENGLISH") }
+    it { expect(defendant.address_1).to eql("Address Line 1") }
+    it { expect(defendant.address_2).to eql("Address Line 2") }
+    it { expect(defendant.address_3).to eql("Address Line 3") }
+    it { expect(defendant.address_4).to eql("Address Line 4") }
+    it { expect(defendant.address_5).to eql("Address Line 5") }
+    it { expect(defendant.postcode).to eql("SW1H 9EA") }
+    it { expect(defendant.phone_home).to eql("000-000-0000") }
+    it { expect(defendant.phone_work).to eql("111-111-1111") }
+    it { expect(defendant.phone_mobile).to eql("222-222-2222") }
+    it { expect(defendant.email_primary).to eql("primary@example.com") }
+    it { expect(defendant.email_secondary).to eql("secondary@example.com") }
+    it { expect(defendant.offence_ids).to eql(%w[b8ffe7db-f994-45a0-9c06-9ec953eeffd7 b8ffe7db-f994-45a0-9c06-9ec953eeff99]) }
+    it { expect(defendant.offences).to all(be_an(HmctsCommonPlatform::Offence)) }
+    it { expect(defendant.judicial_result_ids).to eql(%w[be225605-fc15-47aa-b74c-efb8629db58e]) }
+    it { expect(defendant.judicial_results).to all(be_an(HmctsCommonPlatform::JudicialResult)) }
+    it { expect(defendant.defence_organisation).to be_an(HmctsCommonPlatform::DefenceOrganisation) }
   end
 
   context "when the defendant has only required fields" do
-    let(:data) { JSON.parse(file_fixture("defendant/required_fields.json").read).deep_symbolize_keys }
-
-    it "has an ID" do
-      expect(defendant.id).to eql("5aa2ba71-01d6-481b-82d3-228d61e6e73d")
-    end
+    let(:data) { JSON.parse(file_fixture("defendant/required_fields.json").read) }
 
     it "matches the HMCTS Common Platform schema" do
       expect(data).to match_json_schema(:defendant)
     end
 
-    it "has no proceedings concluded" do
-      expect(defendant.proceedings_concluded).to be_nil
-    end
-
-    it "has no ASN" do
-      expect(defendant.arrest_summons_number).to be_nil
-    end
-
-    it "has no first name" do
-      expect(defendant.first_name).to be_nil
-    end
-
-    it "has no last name" do
-      expect(defendant.last_name).to be_nil
-    end
-
-    it "has no date of birth" do
-      expect(defendant.date_of_birth).to be_nil
-    end
-
-    it "has no NINO" do
-      expect(defendant.nino).to be_nil
-    end
-
-    it "has documentation no language needs" do
-      expect(defendant.documentation_language_needs).to be_nil
-    end
-
-    it "has no address 1" do
-      expect(defendant.address_1).to be_nil
-    end
-
-    it "has no address 2" do
-      expect(defendant.address_2).to be_nil
-    end
-
-    it "has no address 3" do
-      expect(defendant.address_3).to be_nil
-    end
-
-    it "has no address 4" do
-      expect(defendant.address_4).to be_nil
-    end
-
-    it "has no address 5" do
-      expect(defendant.address_5).to be_nil
-    end
-
-    it "has no postcode" do
-      expect(defendant.postcode).to be_nil
-    end
-
-    it "has no home phone" do
-      expect(defendant.phone_home).to be_nil
-    end
-
-    it "has no work phone" do
-      expect(defendant.phone_work).to be_nil
-    end
-
-    it "has no mobile phone" do
-      expect(defendant.phone_mobile).to be_nil
-    end
-
-    it "has no primary email" do
-      expect(defendant.email_primary).to be_nil
-    end
-
-    it "has no secondary email" do
-      expect(defendant.email_secondary).to be_nil
-    end
-
-    it "has no judicial results" do
-      expect(defendant.judicial_results).to be_empty
-    end
+    it { expect(defendant.id).to eql("5aa2ba71-01d6-481b-82d3-228d61e6e73d") }
+    it { expect(data).to match_json_schema(:defendant) }
+    it { expect(defendant.proceedings_concluded).to be_nil }
+    it { expect(defendant.arrest_summons_number).to be_nil }
+    it { expect(defendant.first_name).to be_nil }
+    it { expect(defendant.last_name).to be_nil }
+    it { expect(defendant.date_of_birth).to be_nil }
+    it { expect(defendant.nino).to be_nil }
+    it { expect(defendant.documentation_language_needs).to be_nil }
+    it { expect(defendant.address_1).to be_nil }
+    it { expect(defendant.address_2).to be_nil }
+    it { expect(defendant.address_3).to be_nil }
+    it { expect(defendant.address_4).to be_nil }
+    it { expect(defendant.address_5).to be_nil }
+    it { expect(defendant.postcode).to be_nil }
+    it { expect(defendant.phone_home).to be_nil }
+    it { expect(defendant.phone_work).to be_nil }
+    it { expect(defendant.phone_mobile).to be_nil }
+    it { expect(defendant.email_primary).to be_nil }
+    it { expect(defendant.email_secondary).to be_nil }
+    it { expect(defendant.judicial_results).to be_empty }
 
     describe "offences" do
       it "are HmctsCommonPlatform::Offence objects" do
