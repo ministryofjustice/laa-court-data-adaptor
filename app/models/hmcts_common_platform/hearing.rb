2666d40d57508dc8506df420b265be6496bf5f67
Laurent Curau <laurent.curau@digital.justice.gov.uk>
2022-02-15 16:27:58 +0100

    Hearing #to_json

--- app/models/hmcts_common_platform/hearing.rb
+++ app/models/hmcts_common_platform/hearing.rb
@@ -24,6 +24,20 @@ module HmctsCommonPlatform
       data.dig(:hearingDays, 0, :sittingDay)
     end
 
+    def has_shared_results
+      data[:hasSharedResults]
+    end
+
+    def language
+      data[:hearingLanguage]
+    end
+
+    def hearing_days
+      Array(data[:hearingDays]).map do |hearing_day_data|
+        HmctsCommonPlatform::HearingDay.new(hearing_day_data)
+      end
+    end
+
     def prosecution_cases
       Array(data[:prosecutionCases]).map do |prosecution_case_data|
         HmctsCommonPlatform::ProsecutionCase.new(prosecution_case_data)
@@ -38,6 +52,63 @@ module HmctsCommonPlatform
 
     def court_centre
       HmctsCommonPlatform::CourtCentre.new(data[:courtCentre])
+    end
+
+    def hearing_type
+      HmctsCommonPlatform::HearingType.new(data[:type])
+    end
+
+    def judicial_roles
+      Array(data[:judiciary]).map do |judiciary_data|
+        HmctsCommonPlatform::JudicialRole.new(judiciary_data)
+      end
+    end
+
+    def prosecution_counsels
+      Array(data[:prosecutionCounsels]).map do |prosecution_counsel_data|
+        HmctsCommonPlatform::ProsecutionCounsel.new(prosecution_counsel_data)
+      end
+    end
+
+    def defence_counsels
+      Array(data[:defenceCounsels]).map do |defence_counsel_data|
+        HmctsCommonPlatform::DefenceCounsel.new(defence_counsel_data)
+      end
+    end
+
+    def defendant_judicial_results
+      Array(data[:defendantJudicialResults]).map do |defendant_judicial_result_data|
+        HmctsCommonPlatform::DefendantJudicialResult.new(defendant_judicial_result_data)
+      end
+    end
+
+    def cracked_ineffective_trial
+      HmctsCommonPlatform::CrackedIneffectiveTrial.new(data[:crackedIneffectiveTrial])
+    end
+
+    def to_json(*_args)
+      to_builder.attributes!
+    end
+
+  private
+
+    def to_builder
+      Jbuilder.new do |hearing|
+        hearing.id id
+        hearing.jurisdiction_type jurisdiction_type
+        hearing.court_centre court_centre.to_json
+        hearing.hearing_language language
+        hearing.prosecution_cases prosecution_cases.map(&:to_json)
+        hearing.defendant_judicial_results defendant_judicial_results.map(&:to_json)
+        hearing.has_shared_results has_shared_results
+        hearing.court_applications court_applications.map(&:to_json)
+        hearing.hearing_type hearing_type.to_json
+        hearing.hearing_days hearing_days.map(&:to_json)
+        hearing.judiciary judicial_roles.map(&:to_json)
+        hearing.prosecution_counsels prosecution_counsels.map(&:to_json)
+        hearing.defence_counsels defence_counsels.map(&:to_json)
+        hearing.cracked_ineffective_trial cracked_ineffective_trial.to_json
+      end
     end
   end
 end
--- spec/models/hmcts_common_platform/hearing_spec.rb
+++ spec/models/hmcts_common_platform/hearing_spec.rb
@@ -6,27 +6,37 @@ RSpec.describe HmctsCommonPlatform::Hearing, type: :model do
     expect(data).to match_json_schema(:hearing)
   end
 
-  it "has an ID" do
-    expect(hearing.id).to eql("b935a64a-6d03-4da4-bba6-4d32cc2e7fb4")
+  it "generates a JSON representation of the data" do
+    expect(hearing.to_json["id"]).to eql("b935a64a-6d03-4da4-bba6-4d32cc2e7fb4")
+    expect(hearing.to_json["has_shared_results"]).to be false
+    expect(hearing.to_json["hearing_language"]).to eql("WELSH")
+    expect(hearing.to_json["hearing_type"]).to be_present
+    expect(hearing.to_json["court_centre"]).to be_present
+    expect(hearing.to_json["jurisdiction_type"]).to eql("CROWN")
+    expect(hearing.to_json["cracked_ineffective_trial"]).to be_present
+    expect(hearing.to_json["hearing_days"]).to be_present
+    expect(hearing.to_json["prosecution_cases"]).to be_present
+    expect(hearing.to_json["court_applications"]).to be_present
+    expect(hearing.to_json["prosecution_counsels"]).to be_present
+    expect(hearing.to_json["defence_counsels"]).to be_present
+    expect(hearing.to_json["judiciary"]).to be_present
+    expect(hearing.to_json["defendant_judicial_results"]).to be_present
   end
 
-  it "has a jurisdiction type" do
-    expect(hearing.jurisdiction_type).to eql("CROWN")
-  end
-
-  it "has a court centre ID" do
-    expect(hearing.court_centre_id).to eql("bc4864ca-4b22-3449-9716-a8db1db89905")
-  end
-
-  it "has a first sitting day date" do
-    expect(hearing.first_sitting_day_date).to eql("2019-10-25T10:45:00.000Z")
-  end
-
-  it "has prosecution cases" do
-    expect(hearing.prosecution_cases).to all be_an(HmctsCommonPlatform::ProsecutionCase)
-  end
-
-  it "has court applications" do
-    expect(hearing.court_applications).to all be_an(HmctsCommonPlatform::CourtApplication)
-  end
+  it { expect(hearing.id).to eql("b935a64a-6d03-4da4-bba6-4d32cc2e7fb4") }
+  it { expect(hearing.has_shared_results).to be false }
+  it { expect(hearing.language).to eql("WELSH") }
+  it { expect(hearing.jurisdiction_type).to eql("CROWN") }
+  it { expect(hearing.court_centre_id).to eql("bc4864ca-4b22-3449-9716-a8db1db89905") }
+  it { expect(hearing.first_sitting_day_date).to eql("2019-10-25T10:45:00.000Z") }
+  it { expect(hearing.prosecution_cases).to all be_an(HmctsCommonPlatform::ProsecutionCase) }
+  it { expect(hearing.court_applications).to all be_an(HmctsCommonPlatform::CourtApplication) }
+  it { expect(hearing.judicial_roles).to all be_an(HmctsCommonPlatform::JudicialRole) }
+  it { expect(hearing.prosecution_counsels).to all be_an(HmctsCommonPlatform::ProsecutionCounsel) }
+  it { expect(hearing.defence_counsels).to all be_an(HmctsCommonPlatform::DefenceCounsel) }
+  it { expect(hearing.defendant_judicial_results).to all be_an(HmctsCommonPlatform::DefendantJudicialResult) }
+  it { expect(hearing.hearing_days).to all be_an(HmctsCommonPlatform::HearingDay) }
+  it { expect(hearing.hearing_type).to be_an(HmctsCommonPlatform::HearingType) }
+  it { expect(hearing.cracked_ineffective_trial).to be_an(HmctsCommonPlatform::CrackedIneffectiveTrial) }
+  it { expect(hearing.court_centre).to be_an(HmctsCommonPlatform::CourtCentre) }
 end
