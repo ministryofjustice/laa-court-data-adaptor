c250aa76f5e1b52349cf46fc5efc14f734910ad6
Laurent Curau <laurent.curau@digital.justice.gov.uk>
2022-03-17 14:47:43 +0100

    Specify mandatory fields for POST laa_references

--- spec/requests/api/internal/v1/representation_orders_request_spec.rb
+++ spec/requests/api/internal/v1/representation_orders_request_spec.rb
@@ -119,8 +119,6 @@ RSpec.describe "api/internal/v1/representation_orders", type: :request, swagger_
           let(:Authorization) { "Bearer #{token.token}" }
           before { representation_order[:data][:attributes][:maat_reference] = "ABC123123" }
 
-          parameter "$ref" => "#/components/parameters/transaction_id_header"
-
           before do
             expect(RepresentationOrderCreatorWorker).not_to receive(:perform_async)
           end
@@ -132,8 +130,6 @@ RSpec.describe "api/internal/v1/representation_orders", type: :request, swagger_
       context "when request is unauthorized" do
         response("401", "Unauthorized") do
           let(:Authorization) { nil }
-
-          parameter "$ref" => "#/components/parameters/transaction_id_header"
 
           before do
             expect(RepresentationOrderCreatorWorker).not_to receive(:perform_async)
