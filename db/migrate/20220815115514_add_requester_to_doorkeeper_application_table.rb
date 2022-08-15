a4f5fbe61953878e39e765984db9039095c39e0e
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2022-08-15 14:42:39 +0100

    LASB-338: Reverted deletion of the column type in the requester and requestee migrations.

--- db/migrate/20220815115514_add_requester_to_doorkeeper_application_table.rb
+++ db/migrate/20220815115514_add_requester_to_doorkeeper_application_table.rb
@@ -1,5 +1,5 @@
 class AddRequesterToDoorkeeperApplicationTable < ActiveRecord::Migration[6.1]
   def change
-    add_column :oauth_applications, :requester_email
+    add_column :oauth_applications, :requester_email, :string
   end
 end
--- db/migrate/20220815120308_add_requestee_to_doorkeeper_application_table.rb
+++ db/migrate/20220815120308_add_requestee_to_doorkeeper_application_table.rb
@@ -1,5 +1,5 @@
 class AddRequesteeToDoorkeeperApplicationTable < ActiveRecord::Migration[6.1]
   def change
-    add_column :oauth_applications, :requestee_email
+    add_column :oauth_applications, :requestee_email, :string
   end
 end
