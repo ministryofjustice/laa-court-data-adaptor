ccc7031fe785cc64ca6220f4cd5aa75b87bc6a28
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2022-08-15 14:09:23 +0100

    LASB-338: Corrected syntax for default value in the requester and requestee migrations.

--- db/migrate/20220815115514_add_requester_to_doorkeeper_application_table.rb
+++ db/migrate/20220815115514_add_requester_to_doorkeeper_application_table.rb
@@ -1,5 +1,5 @@
 class AddRequesterToDoorkeeperApplicationTable < ActiveRecord::Migration[6.1]
   def change
-    add_column :oauth_applications, :requester, :string, null: false, :default => ""
+    add_column :oauth_applications, :requester, :string, null: false, default: ""
   end
 end
