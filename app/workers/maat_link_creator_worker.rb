a60078c2f451c71c4022e2fad77ef59462651d8b
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2023-04-19 16:51:44 +0100

    Removed retry mechanism for the create link worker.

--- app/workers/maat_link_creator_worker.rb
+++ app/workers/maat_link_creator_worker.rb
@@ -2,6 +2,7 @@
 
 class MaatLinkCreatorWorker
   include Sidekiq::Worker
+  sidekiq_options retry: 1
 
   def perform(request_id, defendant_id, user_name, maat_reference)
     Current.set(request_id: request_id) do
