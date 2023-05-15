f9e79001fd05a8111721b6e45010b551ea4d7b11
Alessandro De Simone <alexdesi@gmail.com>
2023-05-15 16:15:13 +0100

    LASB-1975: ProsecutionCasesController#index returns 503 Service Unavailable error instead of 200 Success and empty list of entries.

--- spec/services/common_platform/api/search_prosecution_case_spec.rb
+++ spec/services/common_platform/api/search_prosecution_case_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe CommonPlatform::Api::SearchProsecutionCase do
 
   let(:params) { { howdy: "hello" } }
   let(:response_status) { 200 }
-  let(:search_results) { double("Response", status: response_status, body: response_body) }
-  let(:prosecution_case_id) { "5edd67eb-9d8c-44f2-a57e-c8d026defaa4" }
   let(:response_body) { JSON.parse(file_fixture("prosecution_case_search_result.json").read) }
+  let(:common_platform_api_search_results) { double("Response", status: response_status, body: response_body) }
+  let(:prosecution_case_id) { "5edd67eb-9d8c-44f2-a57e-c8d026defaa4" }
 
   before do
-    allow(CommonPlatform::Api::ProsecutionCaseSearcher).to receive(:call).and_return(search_results)
+    allow(CommonPlatform::Api::ProsecutionCaseSearcher).to receive(:call).and_return(common_platform_api_search_results)
   end
 
   it "records ProsecutionCase" do
@@ -58,21 +58,15 @@ RSpec.describe CommonPlatform::Api::SearchProsecutionCase do
     end
   end
 
-  context "when the response is not a 200/ok" do
-    let(:response_status) { 404 }
+  context "when the response is not a 200 Success" do
+    let(:response_status) { 424 }
+    let(:response_body) { "error message" }
 
-    it "does not record" do
-      expect(ProsecutionCaseRecorder).not_to receive(:call)
-      search_prosecution_case
-    end
-  end
-
-  context "when containing HTML" do
-    let(:response_body) { "<html>" }
-
-    it "reports to Sentry" do
-      expect(Sentry).to receive(:capture_message)
-      search_prosecution_case
+    it "raises FailedDependency exception" do
+      expect { search_prosecution_case }.to raise_error(
+        CommonPlatform::Api::Errors::FailedDependency,
+        "body: error message, status: 424",
+      )
     end
   end
 end
