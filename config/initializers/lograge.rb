c2c8b3d52973e144b50175b61bba94aab8d138bb
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-03-12 15:57:20 +0000

    Re-add filtered headers to structured logs

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,3 +1,12 @@
+HEADERS_TO_FILTER = %w[HTTP_AUTHORIZATION].freeze
+
 Rails.application.configure do
   config.lograge.enabled = true
+  config.lograge.custom_options = lambda do |event|
+    {}.tap do |envs|
+      event.payload[:request].headers.to_h.each do |key, value|
+        envs[key] = value if key.downcase.starts_with?("http") && HEADERS_TO_FILTER.exclude?(key)
+      end
+    end
+  end
 end
