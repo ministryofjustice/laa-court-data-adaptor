eaf0e1cb65407a0c463fd9007db1f65fe81431f5
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-03-25 15:31:23 +0000

    Changes following testing against HMCTS dev

--- spec/models/hmcts_common_platform/court_application_summary_spec.rb
+++ spec/models/hmcts_common_platform/court_application_summary_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe HmctsCommonPlatform::CourtApplicationSummary, type: :model do
     expect(court_application_summary.to_json["received_date"]).to eql("2023-06-27")
     expect(court_application_summary.to_json["case_summary"]).to be_a(Array)
     expect(court_application_summary.to_json["hearing_summary"]).to be_a(Array)
-    expect(court_application_summary.to_json["subject_summary"]).to be_a(Array)
+    expect(court_application_summary.to_json["subject_summary"]).to be_a(Hash)
   end
 
   it { expect(court_application_summary.application_id).to eql("00004c9f-af9f-401a-b88b-78a4f0e08163") }
@@ -22,5 +22,21 @@ RSpec.describe HmctsCommonPlatform::CourtApplicationSummary, type: :model do
   it { expect(court_application_summary.received_date).to eql("2023-06-27") }
   it { expect(court_application_summary.case_summary.first).to be_a(HmctsCommonPlatform::CaseSummary) }
   it { expect(court_application_summary.hearing_summary.first).to be_a(HmctsCommonPlatform::HearingSummary) }
-  it { expect(court_application_summary.subject_summary.first).to be_a(HmctsCommonPlatform::SubjectSummary) }
+  it { expect(court_application_summary.subject_summary).to be_a(HmctsCommonPlatform::SubjectSummary) }
+
+  context "when data is presented as a string" do
+    let(:data) { file_fixture("court_application_summary.json").read }
+
+    it "generates a transformed JSON representation of the data" do
+      expect(court_application_summary.to_json["application_id"]).to eql("00004c9f-af9f-401a-b88b-78a4f0e08163")
+      expect(court_application_summary.to_json["application_reference"]).to eql("29GD7216523")
+      expect(court_application_summary.to_json["application_status"]).to eql("FINALISED")
+      expect(court_application_summary.to_json["application_title"]).to eql("Appeal against conviction by a Magistrates' Court to the Crown Court")
+      expect(court_application_summary.to_json["application_type"]).to eql("Appeal against conviction by a Magistrates' Court to the Crown Court")
+      expect(court_application_summary.to_json["received_date"]).to eql("2023-06-27")
+      expect(court_application_summary.to_json["case_summary"]).to be_a(Array)
+      expect(court_application_summary.to_json["hearing_summary"]).to be_a(Array)
+      expect(court_application_summary.to_json["subject_summary"]).to be_a(Hash)
+    end
+  end
 end
