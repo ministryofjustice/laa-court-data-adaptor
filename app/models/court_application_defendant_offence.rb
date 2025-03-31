f14b83621c36e6cb04e8c812c69c2b85c74e42be
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-03-31 16:21:04 +0100

    Add default scopes to stop us loading the wrong record at the wrong time

--- app/models/court_application_defendant_offence.rb
+++ app/models/court_application_defendant_offence.rb
@@ -2,4 +2,8 @@ class CourtApplicationDefendantOffence < LegalCaseDefendantOffence
   alias_attribute :court_application_id, :prosecution_case_id
   validates :court_application_id, presence: true
   belongs_to :court_application
+
+  # Application types are only relevant to court applications,
+  # so we can use their presence to weed out court applications
+  default_scope { where.not(application_type: nil) }
 end
--- app/models/prosecution_case_defendant_offence.rb
+++ app/models/prosecution_case_defendant_offence.rb
@@ -3,4 +3,8 @@
 class ProsecutionCaseDefendantOffence < LegalCaseDefendantOffence
   validates :prosecution_case_id, presence: true
   belongs_to :prosecution_case
+
+  # Application types are only relevant to court applications,
+  # so we can use their presence to weed out court applications
+  default_scope { where(application_type: nil) }
 end
