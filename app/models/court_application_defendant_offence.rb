dc8449b5ac8116e707b89ee1ffd87dabecc51b57
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-04-01 10:49:02 +0100

    Fix config of CADOs

--- app/models/court_application_defendant_offence.rb
+++ app/models/court_application_defendant_offence.rb
@@ -1,7 +1,9 @@
 class CourtApplicationDefendantOffence < LegalCaseDefendantOffence
+  belongs_to :court_application, foreign_key: :prosecution_case_id
+
   alias_attribute :court_application_id, :prosecution_case_id
   validates :court_application_id, presence: true
-  belongs_to :court_application
+  validates :application_type, presence: true
 
   # Application types are only relevant to court applications,
   # so we can use their presence to weed out court applications
