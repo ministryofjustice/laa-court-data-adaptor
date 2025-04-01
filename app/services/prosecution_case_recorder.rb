6679ff304e2063574bf7e33ee7881db6e6a04e4a
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-04-01 10:37:05 +0100

    Remove redundant clause

--- app/services/prosecution_case_recorder.rb
+++ app/services/prosecution_case_recorder.rb
@@ -15,7 +15,6 @@ class ProsecutionCaseRecorder < ApplicationService
           prosecution_case_id: prosecution_case.id,
           defendant_id: defendant.id,
           offence_id: offence.id,
-          application_type: nil,
         )
       end
     end
