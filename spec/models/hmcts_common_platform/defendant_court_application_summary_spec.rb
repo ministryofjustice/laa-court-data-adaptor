cff9f439796589444f83535b847204e2ec0dbee3
mo-zag <mo-zag@users.noreply.github.com>
2025-04-15 13:35:12 +0100

    Return ACD-522 application_summaries within defendant_summaries payload
    ACD-652 match defendant to a application vis firstName and lastName and append the matched application within defendant_summaries so VCD can now just display the application title.

---
+++ spec/models/hmcts_common_platform/defendant_court_application_summary_spec.rb
@@ -0,0 +1,45 @@
+RSpec.describe HmctsCommonPlatform::DefendantCourtApplicationSummary, type: :model do
+  let(:defendant_court_application_summary) { described_class.new(data) }
+
+  context "with all fields" do
+    let(:data) { JSON.parse(file_fixture("defendant_court_application_summary/all_fields.json").read) }
+
+    it "matches the schema" do
+      expect(data).to match_json_schema(:defendant_court_application_summary)
+    end
+
+    it { expect(defendant_court_application_summary.id).to eql("9aca847f-da4e-444b-8f5a-2ce7d776ab75") }
+    it { expect(defendant_court_application_summary.short_id).to eql("A25ULRHLVC7S") }
+    it { expect(defendant_court_application_summary.reference).to eql("03811314") }
+    it { expect(defendant_court_application_summary.title).to eql("Appeal against conviction") }
+    it { expect(defendant_court_application_summary.received_date).to eql("2025-04-11") }
+  end
+
+  context "with only required fields" do
+    let(:data) { JSON.parse(file_fixture("defendant_court_application_summary/required_fields.json").read) }
+
+    it "matches the HMCTS Common Platform schema" do
+      expect(data).to match_json_schema(:defendant_court_application_summary)
+    end
+
+    it { expect(defendant_court_application_summary.id).to eql("9aca847f-da4e-444b-8f5a-2ce7d776ab75") }
+    it { expect(defendant_court_application_summary.short_id).to eql("A25ULRHLVC7S") }
+    it { expect(defendant_court_application_summary.reference).to eql("03811314") }
+    it { expect(defendant_court_application_summary.title).to eql("Appeal against conviction") }
+    it { expect(defendant_court_application_summary.received_date).to eql("2025-04-11") }
+  end
+
+  describe "#to_json" do
+    let(:data) { JSON.parse(file_fixture("defendant_court_application_summary/all_fields.json").read) }
+
+    it "generates a JSON representation of the data" do
+      json = defendant_court_application_summary.to_json
+
+      expect(json["id"]).to eql("9aca847f-da4e-444b-8f5a-2ce7d776ab75")
+      expect(json["short_id"]).to eql("A25ULRHLVC7S")
+      expect(json["reference"]).to eql("03811314")
+      expect(json["title"]).to eql("Appeal against conviction")
+      expect(json["received_date"]).to eql("2025-04-11")
+    end
+  end
+end
