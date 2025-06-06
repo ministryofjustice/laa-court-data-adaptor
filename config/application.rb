ceb7f13087df44dba7d78a5471dbc03319db488c
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-06-06 09:28:00 +0100

    Remove some warnings

--- config/application.rb
+++ config/application.rb
@@ -66,5 +66,6 @@ module LaaCourtDataAdaptor
     config.session_store :cookie_store, key: "_interslice_session"
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
+    config.active_support.to_time_preserves_timezone = :zone
   end
 end
