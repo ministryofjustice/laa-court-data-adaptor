509e3d7ae81b1ba0e579c32b3b1cc64416fe67c2
Alessandro De Simone <alexdesi@gmail.com>
2025-06-13 09:56:37 +0100

    Make sync calls for link/unlink instead async.

--- app/controllers/api/internal/v2/court_application_laa_references_controller.rb
+++ app/controllers/api/internal/v2/court_application_laa_references_controller.rb
@@ -6,15 +6,27 @@ module Api
           contract = CourtApplicationLaaReferenceContract.new.call(**transformed_params)
           enforce_contract!(contract)
 
-          enqueue_link
-          head :accepted
+          CourtApplicationMaatLinkCreator.call(
+            transformed_params[:subject_id],
+            transformed_params[:user_name],
+            transformed_params[:maat_reference]
+          )
+
+          head :created
         end
 
         def update
           contract = UnlinkCourtApplicationSubjectContract.new.call(**transformed_params)
           enforce_contract!(contract)
 
-          enqueue_unlink
+          CourtApplicationLaaReferenceUnlinker.call(
+            subject_id: transformed_params[:subject_id],
+            user_name: transformed_params[:user_name],
+            unlink_reason_code: transformed_params[:unlink_reason_code],
+            unlink_other_reason_text: transformed_params[:unlink_other_reason_text],
+            maat_reference: transformed_params[:maat_reference]
+          )
+
           head :accepted
         end
 
@@ -24,28 +36,6 @@ module Api
           unless contract.success?
             raise Errors::ContractError.new(contract, "Contract")
           end
-        end
-
-        def enqueue_link
-          CourtApplicationMaatLinkCreatorWorker.perform_async(
-            Current.request_id,
-            transformed_params[:subject_id],
-            transformed_params[:user_name],
-            transformed_params[:maat_reference],
-          )
-        end
-
-        def enqueue_unlink
-          check_defendant_presence!
-
-          UnlinkCourtApplicationLaaReferenceWorker.perform_async(
-            Current.request_id,
-            transformed_params[:subject_id],
-            transformed_params[:user_name],
-            transformed_params[:unlink_reason_code],
-            transformed_params[:unlink_other_reason_text],
-            transformed_params[:maat_reference],
-          )
         end
 
         def check_defendant_presence!
