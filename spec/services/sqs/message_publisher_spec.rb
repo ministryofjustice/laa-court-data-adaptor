199e1c64486be5d2541b715ec0b725f623ea8936
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-07-15 15:08:12 +0100

    Strip out the unicode characters SQS cannot handle

--- spec/services/sqs/message_publisher_spec.rb
+++ spec/services/sqs/message_publisher_spec.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe Sqs::MessagePublisher do
-  subject(:publish) { described_class.call(message: { blah: "blah" }, sqs_client: client_double) }
+  subject(:publish) { described_class.call(message:, sqs_client: client_double) }
 
   let(:client_double) { instance_double(Aws::SQS::Client) }
+  let(:message) { { blah: "blah" } }
 
   it "does not publish a message" do
     expect(client_double).not_to receive(:send_message)
@@ -11,7 +12,7 @@ RSpec.describe Sqs::MessagePublisher do
   end
 
   context "when the queue url is provided" do
-    subject(:publish) { described_class.call(message: { blah: "blah" }, queue_url: "/fancy-sqs-url", sqs_client: client_double) }
+    subject(:publish) { described_class.call(message:, queue_url: "/fancy-sqs-url", sqs_client: client_double) }
 
     before do
       allow(Current).to receive(:request_id).and_return("XYZ")
@@ -24,6 +25,18 @@ RSpec.describe Sqs::MessagePublisher do
     it "publishes a message to the sqs queue as json" do
       expect(client_double).to receive(:send_message).with(queue_url: "/fancy-sqs-url", message_body: json_message_body)
       publish
+    end
+
+    context "when message contains invalid characters" do
+      let(:message) { { blah: "â’r \uFFFFcloud\uFFFF 🐱‍💻" } }
+
+      it "publishes a sanitized version of the message" do
+        expect(client_double).to receive(:send_message).with(
+          queue_url: "/fancy-sqs-url",
+          message_body: '{"blah":"â’r cloud 🐱‍💻","metadata":{"laaTransactionId":"XYZ"}}',
+        )
+        publish
+      end
     end
   end
 end
