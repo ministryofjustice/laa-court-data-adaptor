b7cd3ed262c85196d653ab3357212d271e463eb1
Alessandro De Simone <alexdesi@gmail.com>
2025-12-11 18:58:58 +0000

    Fix linking for breaches.
    In case of breaches, caseSummary is empty, so the case_status will be read from the ProsecutionCase.

--- app/models/prosecution_case.rb
+++ app/models/prosecution_case.rb
@@ -49,6 +49,10 @@ class ProsecutionCase < ApplicationRecord
                                       .select(&:category_supported?)
   end
 
+  def case_status
+    body["caseStatus"]
+  end
+
 private
 
   def case_details
