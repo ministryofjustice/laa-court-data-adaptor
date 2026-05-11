bcafd3cbcdcd9a06c0aede9b189d26f158f00585
Alessandro De Simone <alexdesi@gmail.com>
2026-05-11 17:33:07 +0100

    ACD-1351: improve import errors CSV and tidy validations

--- app/services/import_xhibit_cases.rb
+++ app/services/import_xhibit_cases.rb
@@ -6,13 +6,13 @@ class ImportXhibitCases < ApplicationService
   end
 
   def call
-    results = { inserted: [], errors: [] }
-    CSV.foreach(file_path, headers: true).with_index(2) do |row, line|
+    results = { success_count: 0, errors: [] }
+    CSV.foreach(file_path, headers: true).with_index(2) do |row, line_number|
       xhibit_case = XhibitMigratedCase.create(row.to_h.transform_values(&:presence))
       if xhibit_case.persisted?
-        results[:inserted] << { line:, case_urn: xhibit_case.case_urn }
+        results[:success_count] += 1
       else
-        results[:errors] << { line:, case_urn: row["case_urn"], messages: xhibit_case.errors.full_messages }
+        results[:errors] << { line_number:, case_urn: row["case_urn"], row: row.to_h, messages: xhibit_case.errors.full_messages }
       end
     end
     results
