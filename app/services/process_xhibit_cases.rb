460404f72cdf8eceeeddd00e8a79c760ba1ed76e
Leandro Costantini <leandro.costantini@justice.gov.uk>
2026-08-07 17:17:32 +0100

    [ACD-1356] Make changes to `ProcessXhibitCases` in order to send all params to `MaatApplicationSearcher`

--- app/services/process_xhibit_cases.rb
+++ app/services/process_xhibit_cases.rb
@@ -4,31 +4,52 @@ class ProcessXhibitCases < ApplicationService
 
   def call
     XhibitMigratedCase.pending.find_each do |xhibit_case|
-      process(xhibit_case)
+      process_case(xhibit_case)
     end
   end
 
 private
 
-  def process(xhibit_case)
-    response = MaatApi::MaatApplicationSearcher.call(
-      first_name: xhibit_case.defendant_first_name,
-      last_name: xhibit_case.defendant_last_name,
-    )
+  def process_case(xhibit_case)
+    response = maat_search(xhibit_case)
 
-    case response&.status
-    when SUCCESS
-      # TODO: process the maat applications matching the case.
-    when NOT_FOUND
-      xhibit_case.action_required!
-    else
-      record_error(xhibit_case, status: response&.status, body: response&.body)
-    end
+    handle_response(response, xhibit_case)
   rescue Faraday::Error => e
     record_error(xhibit_case, error: e.class.name, message: e.message)
   end
 
+  def maat_search(xhibit_case)
+    MaatApi::MaatApplicationSearcher.call(
+      first_name: xhibit_case.defendant_first_name,
+      last_name: xhibit_case.defendant_last_name,
+      date_of_birth: xhibit_case.defendant_date_of_birth,
+      arrest_summons_number: xhibit_case.defendant_arrest_summons_number,
+      committal_date: xhibit_case.committal_date,
+      case_type: xhibit_case.case_type,
+    )
+  end
+
+  def handle_response(response, xhibit_case)
+    case response&.status
+    when SUCCESS
+      handle_success(response, xhibit_case)
+    when NOT_FOUND
+      handle_not_found(xhibit_case)
+    else
+      record_error(xhibit_case, error: response&.status, message: response&.body)
+    end
+  end
+
+  def handle_success(_response, _xhibit_case)
+    # TODO: process the maat applications matching the case.
+  end
+
+  def handle_not_found(xhibit_case)
+    xhibit_case.action_required!
+    record_error(xhibit_case, message: "MAAT application not found")
+  end
+
   def record_error(xhibit_case, **process_errors)
-    xhibit_case.update!(process_errors:)
+    xhibit_case.update!(process_errors: process_errors)
   end
 end
--- spec/services/process_xhibit_cases_spec.rb
+++ spec/services/process_xhibit_cases_spec.rb
@@ -3,45 +3,56 @@ require "rails_helper"
 RSpec.describe ProcessXhibitCases do
   subject(:process_cases) { described_class.call }
 
-  let(:cassette) { "maat_api/search_maat_application_not_found" }
-  let!(:xhibit_case) do
-    create_case(first_name: "nonexistent-first-name", last_name: "nonexistent-last-name")
-  end
-
-  around do |example|
-    VCR.use_cassette(cassette,
-                     tag: :maat_api,
-                     match_requests_on: %i[method uri body]) do
-      example.run
+  shared_context "with maat api cassette" do
+    around do |example|
+      VCR.use_cassette(cassette,
+                       tag: :maat_api,
+                       match_requests_on: %i[method uri]) do
+        example.run
+      end
     end
   end
 
-  it "set status to (manual) action_required when there is no matching maat application" do
-    process_cases
+  context "when there is no matching maat application" do
+    include_context "with maat api cassette"
 
-    expect(xhibit_case.reload).to be_action_required
+    let(:cassette) { "maat_api/search_maat_application_not_found" }
+    let!(:xhibit_case) { create_case(first_name: "nonexistent-first-name", last_name: "nonexistent-last-name") }
+
+    before { process_cases }
+
+    it "sets status to (manual) action_required" do
+      expect(xhibit_case.reload).to be_action_required
+    end
+
+    it "stores a message on the case" do
+      expect(xhibit_case.reload.process_errors).to eq(
+        "message" => "MAAT application not found",
+      )
+    end
   end
 
   context "when there is a matching maat application" do
+    include_context "with maat api cassette"
+
     let(:cassette) { "maat_api/search_maat_application_success" }
-    let!(:xhibit_case) { create_case(first_name: "Tango", last_name: "JF-LAA-T") }
+    let(:xhibit_case) { create_case(first_name: "Tango", last_name: "JF-LAA-T") }
 
     it "process - WIP", skip: "processing a matching maat application is not implemented yet" do
       process_cases
-
-      # TODO: this case is NOT yet implemented!
     end
   end
 
   context "when the search fails" do
+    let!(:xhibit_case) { create_case(first_name: "Tango", last_name: "JF-LAA-T") }
+
     before do
       allow(MaatApi::MaatApplicationSearcher).to receive(:call)
         .and_raise(Faraday::ConnectionFailed, "connection refused")
+      process_cases
     end
 
     it "stores the error on the case" do
-      process_cases
-
       expect(xhibit_case.reload.process_errors).to eq(
         "error" => "Faraday::ConnectionFailed", "message" => "connection refused",
       )
