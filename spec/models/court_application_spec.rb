1aa110815cc5f205582e8f2e9fb63c663311cf86
Stuart Harrison <pezholio@gmail.com>
2026-08-17 11:22:06 +0100

    Add a `has_offences?` method to `CourtApplication`
    This checks the body of the offence and returns true if there are
    any offences contained within the `offenceSummary` array

--- spec/models/court_application_spec.rb
+++ spec/models/court_application_spec.rb
@@ -1,17 +1,71 @@
 # frozen_string_literal: true
 
 RSpec.describe CourtApplication, type: :model do
-  subject(:court_application) { described_class.new(body: { applicationType: application_type }) }
+  let(:court_application) { described_class.new(body:) }
 
-  context "when category is appeal" do
-    let(:application_type) { "MC80801" }
+  describe "appeal?" do
+    subject { court_application.appeal? }
 
-    it { expect(court_application.appeal?).to be(true) }
+    let(:body) { { applicationType: application_type } }
+
+    context "when category is appeal" do
+      let(:application_type) { "MC80801" }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context "when category is breach" do
+      let(:application_type) { "CJ03510" }
+
+      it { is_expected.to be_falsey }
+    end
   end
 
-  context "when category is breach" do
-    let(:application_type) { "CJ03510" }
+  describe "has_offences?" do
+    subject { court_application.has_offences? }
 
-    it { expect(court_application.appeal?).to be(false) }
+    context "when offenceSummary contains an offence" do
+      let(:body) do
+        {
+          subjectSummary: {
+            offenceSummary: [
+              {
+                something: "here",
+              },
+            ],
+          },
+        }
+      end
+
+      it { is_expected.to be_truthy }
+    end
+
+    context "when offenceSummary is empty" do
+      let(:body) do
+        {
+          subjectSummary: {
+            offenceSummary: [],
+          },
+        }
+      end
+
+      it { is_expected.to be_falsey }
+    end
+
+    context "when offenceSummary does not exist" do
+      let(:body) do
+        {
+          subjectSummary: {},
+        }
+      end
+
+      it { is_expected.to be_falsey }
+    end
+
+    context "when subjectSummary does not exist" do
+      let(:body) { {} }
+
+      it { is_expected.to be_falsey }
+    end
   end
 end
