58afcc1649be3c43c6f119aaf730bc03af082179
Alessandro De Simone <alexdesi@gmail.com>
2026-08-28 15:06:31 +0100

    LAACONNECT-1357: Fetch court data from Common Platform before linking
    Search Common Platform by case URN so the prosecution case and its offences
    are recorded locally, as both MAAT link creators read from the database.
    
    Select the link creator by case type: T links the prosecution case by
    defendant_id, S and A link the court application by subject_id.

--- app/services/link_xhibit_case.rb
+++ app/services/link_xhibit_case.rb
@@ -1,27 +1,43 @@
 class LinkXhibitCase < ApplicationService
-  class LinkFailure < StandardError; end
+  TRIAL = "T".freeze
+  SENTENCE = "S".freeze
+  APPEAL = "A".freeze
+  USER_NAME = "SYSTEM".freeze
 
-  def call(maat_search_response, xhibit_case)
-    create_maat_link!(xhibit_case.defendant_id, maat_search_response.maat_id)
-    update_xhibit_case!(xhibit_case, maat_search_response.maat_id)
+  def initialize(maat_search_response, xhibit_case, court_data)
+    @maat_search_response = maat_search_response
+    @xhibit_case = xhibit_case
+    @court_data = court_data
+  end
+
+  def call
+    case xhibit_case.case_type
+    when TRIAL
+      ProsecutionCaseMaatLinkCreator.call(court_data.defendant_id, USER_NAME, maat_id)
+    when SENTENCE, APPEAL
+      CourtApplicationMaatLinkCreator.call(subject_id, USER_NAME, maat_id)
+    else
+      raise ArgumentError, "Unsupported case type: #{xhibit_case.case_type.inspect}"
+    end
+
+    xhibit_case.update!(
+      maat_id:,
+      status: :auto_linked,
+      linked_at: Time.zone.now,
+      linked_by: USER_NAME,
+    )
   end
 
 private
 
-  def create_maat_link!(defendant_id, maat_id)
-    CourtApplicationMaatLinkCreator.call(
-      defendant_id,
-      "SYSTEM",
-      maat_id,
-    )
+  attr_reader :maat_search_response, :xhibit_case, :court_data
+
+  def subject_id
+    court_data.application_summaries.first&.subject_summary&.subject_id ||
+      raise(ArgumentError, "No court application found for defendant #{court_data.defendant_id}")
   end
 
-  def update_xhibit_case!(xhibit_case, maat_id)
-    xhibit_case.update!(
-      maat_id: maat_id,
-      status: :auto_linked,
-      linked_at: Time.zone.now,
-      linked_by: "SYSTEM",
-    )
+  def maat_id
+    maat_search_response.maat_id
   end
 end
--- spec/services/link_xhibit_case_spec.rb
+++ spec/services/link_xhibit_case_spec.rb
@@ -1,13 +1,25 @@
 RSpec.describe LinkXhibitCase, type: :service do
   describe "#call" do
     let(:maat_response) { instance_double(MaatApi::SearchResponse, maat_id: "6559879") }
+    let(:case_type) { "T" }
+    let(:application_summaries) { [] }
+    let(:subject_id) { "b7c8e2f0-0000-4000-8000-000000000002" }
+
+    let(:court_data) do
+      instance_double(
+        HmctsCommonPlatform::DefendantSummary,
+        defendant_id: "b7c8e2f0-0000-4000-8000-000000000001",
+        application_summaries:,
+      )
+    end
+
     let(:xhibit_case) do
       XhibitMigratedCase.create!(
         case_urn: "20GD021701",
         xhibit_case_number: "T202540001",
         court_name: "Derby Justice Centre",
         ou_code: "B30PI00",
-        case_type: "T",
+        case_type:,
         defendant_id: "defendant-1",
         defendant_first_name: "Alice",
         defendant_last_name: "Smith",
@@ -15,9 +27,10 @@ RSpec.describe LinkXhibitCase, type: :service do
       )
     end
 
-    let(:link_case) { described_class.new.call(maat_response, xhibit_case) }
+    let(:link_case) { described_class.call(maat_response, xhibit_case, court_data) }
 
     before do
+      allow(ProsecutionCaseMaatLinkCreator).to receive(:call)
       allow(CourtApplicationMaatLinkCreator).to receive(:call)
     end
 
@@ -28,14 +41,52 @@ RSpec.describe LinkXhibitCase, type: :service do
                           .and change { xhibit_case.reload.linked_at }.from(nil).to(within(1.second).of(Time.zone.now))
     end
 
-    it "calls the CourtApplicationMaatLinkCreator" do
+    it "links a trial using the defendant id" do
       link_case
 
-      expect(CourtApplicationMaatLinkCreator).to have_received(:call).with(
-        xhibit_case.defendant_id,
-        "SYSTEM",
-        maat_response.maat_id,
+      expect(ProsecutionCaseMaatLinkCreator).to have_received(:call).with(
+        court_data.defendant_id, "SYSTEM", maat_response.maat_id
       )
+    end
+
+    %w[A S].each do |court_application_case_type|
+      context "when the case type is #{court_application_case_type}" do
+        let(:case_type) { court_application_case_type }
+        let(:application_summaries) do
+          [
+            instance_double(
+              HmctsCommonPlatform::ApplicationSummary,
+              subject_summary: instance_double(HmctsCommonPlatform::SubjectSummary, subject_id:),
+            ),
+          ]
+        end
+
+        it "links the court application using the subject id" do
+          link_case
+
+          expect(CourtApplicationMaatLinkCreator).to have_received(:call).with(
+            subject_id, "SYSTEM", maat_response.maat_id
+          )
+        end
+      end
+    end
+
+    context "when a court application case has no matching application" do
+      let(:case_type) { "A" }
+
+      it "raises rather than marking the case linked" do
+        expect { link_case }.to raise_error(ArgumentError, /No court application found/)
+        expect(xhibit_case.reload).to be_pending
+      end
+    end
+
+    context "when the case type is not recognised" do
+      let(:case_type) { "X" }
+
+      it "raises rather than marking the case linked" do
+        expect { link_case }.to raise_error(ArgumentError, /Unsupported case type/)
+        expect(xhibit_case.reload).to be_pending
+      end
     end
   end
 end
