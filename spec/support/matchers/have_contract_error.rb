5577d4257e262cfb330c55cb68f6acee50a37b58
Stuart Harrison <pezholio@gmail.com>
2026-09-10 13:54:25 +0100

    Fix typo
    Noticed that `fullfilment` was mispelled in our new matcher, so fixed
    the mispelling in other places too!

--- spec/support/matchers/have_contract_error.rb
+++ spec/support/matchers/have_contract_error.rb
@@ -7,22 +7,22 @@ require "rspec/expectations"
 # expect(contract.call(my_integer: '1')).to have_contract_error('must be an integer')
 #
 RSpec::Matchers.define :have_contract_error do |message|
-  match do |fullfillment|
-    fullfillment.errors.map(&:text).any? { |msg| msg.match?(Regexp.escape(message)) }
+  match do |fullfilment|
+    fullfilment.errors.map(&:text).any? { |msg| msg.match?(Regexp.escape(message)) }
   end
 
   description do
     "have contract error"
   end
 
-  failure_message do |fullfillment|
-    errors = fullfillment.errors.to_h
-    "expected contract fullfillment errors to include message: \"#{message}\"\n" \
+  failure_message do |fullfilment|
+    errors = fullfilment.errors.to_h
+    "expected contract fullfilment errors to include message: \"#{message}\"\n" \
       "but received: #{errors.empty? ? 'none' : errors}"
   end
 
-  failure_message_when_negated do |fullfillment|
-    "expected contract fullfillment not to include error message \"#{message}\"\n" \
-      "but it was included in #{fullfillment.errors.to_h}"
+  failure_message_when_negated do |fullfilment|
+    "expected contract fullfilment not to include error message \"#{message}\"\n" \
+      "but it was included in #{fullfilment.errors.to_h}"
   end
 end
