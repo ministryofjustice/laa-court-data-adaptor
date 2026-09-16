f1f17068e697b50bf48203d7ad9a50a02e994f62
Leandro Costantini <leandro.costantini@justice.gov.uk>
2026-09-16 16:46:49 +0100

    [LAACONNECT-1527] Catch 500 responses retries in the specs

--- spec/services/common_platform/connection_spec.rb
+++ spec/services/common_platform/connection_spec.rb
@@ -59,7 +59,7 @@ RSpec.describe CommonPlatform::Connection do
       retry_options = {
         retry_statuses: [409, 429, 500, 502, 504],
         max: 3,
-        interval: 0.05,
+        interval: 3,
         interval_randomness: 0.5,
         backoff_factor: 2,
         methods: %i[delete get head options put post],
