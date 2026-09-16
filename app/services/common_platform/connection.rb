f2f366e55e1c1010edf1bacec2259c788afad931
Leandro Costantini <leandro.costantini@justice.gov.uk>
2026-09-16 10:15:42 +0100

    [LAACONNECT-1527] Fix Sentry events `CommonPlatform::Api::Errors::FailedDependency`

--- app/services/common_platform/connection.rb
+++ app/services/common_platform/connection.rb
@@ -83,9 +83,13 @@ module CommonPlatform
 
     def retry_options
       {
-        retry_statuses: [429],
+        retry_statuses: [409, 429, 500, 502, 504],
+        max: 3,
         interval: 3,
-        methods: %i[delete get head options put post],
+        interval_randomness: 0.5,
+        backoff_factor: 2,
+        methods: Faraday::Retry::Middleware::IDEMPOTENT_METHODS + [:post],
+        exceptions: Faraday::Retry::Middleware::DEFAULT_EXCEPTIONS + [Faraday::ConnectionFailed, Faraday::ParsingError],
       }
     end
   end
