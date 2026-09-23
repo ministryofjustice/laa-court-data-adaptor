4969b07fb6c421a8f08a01b89e5454d0671b08ad
Leandro Costantini <leandro.costantini@justice.gov.uk>
2026-09-23 23:31:37 +0100

    [LAACONNECT-1527] Tweak common platform connection adaptor settings

--- spec/services/common_platform/connection_spec.rb
+++ spec/services/common_platform/connection_spec.rb
@@ -15,7 +15,10 @@ RSpec.describe CommonPlatform::Connection do
   let(:client_key) { nil }
 
   let(:request_options) do
-    { headers: { "Ocp-Apim-Subscription-Key" => "super-secret-key" } }
+    {
+      headers: { "Ocp-Apim-Subscription-Key" => "super-secret-key" },
+      request: { open_timeout: 5, read_timeout: 30, write_timeout: 10 },
+    }
   end
 
   it "connects to the common platform url" do
@@ -31,6 +34,7 @@ RSpec.describe CommonPlatform::Connection do
         headers: {
           "Ocp-Apim-Subscription-Key" => "super-secret-key",
         },
+        request: { open_timeout: 5, read_timeout: 30, write_timeout: 10 },
         ssl: {
           client_cert: "OPENSSL_CERT",
           client_key: "OPENSSL_KEY",
@@ -67,22 +71,66 @@ RSpec.describe CommonPlatform::Connection do
         exceptions: Faraday::Retry::Middleware::DEFAULT_EXCEPTIONS + [Faraday::ConnectionFailed, Faraday::ParsingError],
       }
 
-      expect(connection).to receive(:request).with(:retry, retry_options)
+      expect(connection).to receive(:use).with(CommonPlatform::Connection::FailureMiddleware).ordered
+      expect(connection).to receive(:request).with(:retry, retry_options).ordered
       expect(connection).to receive(:request).with(:json)
-      expect(connection).to receive(:use).with(CommonPlatform::Connection::FailureMiddleware)
       expect(connection).to receive(:response).with(:logger, TaggedLogger, { headers: false, formatter: CommonPlatform::Connection::LogFormatter })
       expect(connection).to receive(:response).with(:json, content_type: "application/json")
       expect(connection).to receive(:response).with(:json, content_type: "application/vnd.unifiedsearch.query.laa.cases+json")
       expect(connection).to receive(:response).with(:json, content_type: "text/plain")
-      expect(connection).to receive(:adapter).with(:net_http_persistent, {
-        idle_timeout: 120,
-        keep_alive: 60,
-        pool_size: 10,
-        open_timeout: 3,
-        read_timeout: 10,
-      })
+      expect(connection).to receive(:adapter).with(:net_http_persistent, pool_size: described_class::POOL_SIZE)
 
       connect_to_common_platform
+    end
+  end
+
+  describe "failed connection settings" do
+    let(:host) { "https://example.com" }
+
+    describe "timeouts" do
+      it "applies them to the Net::HTTP::Persistent connection" do
+        adapter = connect_to_common_platform.builder.adapter.build(nil)
+        env = Faraday::Env.new
+        env[:url] = URI(host)
+        env[:request] = connect_to_common_platform.options
+        env[:ssl] = Faraday::SSLOptions.new
+
+        http = adapter.send(:build_connection, env)
+
+        expect(http.open_timeout).to eq(5)
+        expect(http.read_timeout).to eq(30)
+        expect(http.write_timeout).to eq(10)
+        expect(http.idle_timeout).to eq(5)
+      end
+    end
+
+    describe "connection pool" do
+      it "is at least as large as the thread pool it serves" do
+        expect(described_class::POOL_SIZE).to be > described_class::MAX_THREADS
+      end
+    end
+
+    describe "retries" do
+      it "retries connection failures before raising an error" do
+        attempts = 0
+        stubs = Faraday::Adapter::Test::Stubs.new do |stub|
+          stub.get("/anything") do
+            attempts += 1
+            raise Faraday::ConnectionFailed, "rush hour"
+          end
+        end
+
+        # Stubbing here so the example does not actually sleep around 7 seconds
+        allow(Faraday::Retry::Middleware).to receive(:new).and_wrap_original do |original, *args, **kwargs, &blk|
+          original.call(*args, **kwargs, &blk).tap { |middleware| allow(middleware).to receive(:sleep) }
+        end
+
+        connection = connect_to_common_platform
+        connection.builder.adapter(:test, stubs)
+
+        expect { connection.get("/anything") }.to raise_error(CommonPlatform::Api::Errors::FailedDependency)
+        expect(attempts).to eq(4)
+      end
     end
   end
 end
